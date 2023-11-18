4e4b17d5d5f5fb90a1649badd7be968f5db820c0
Evan0A <141932923+Evan0A@users.noreply.github.com>
2023-11-18 17:51:36 +0700

    Update cihuy hub.lua
    panco sc

--- cihuy hub.lua
+++ cihuy hub.lua
@@ -1,1 +1,19 @@
+autoTap = false
+
+local function loadGame()
+  while not game:IsLoad() do
+    wait()
+  until game:Isloaded()
+  return true
+end
+
+function Tap()
+  if loadGame() then
+    while autoTap then
+    game:GetService("ReplicatedStorage").Packages.Knit.Services.ToolService.RE.onClick:FireServer()
+    wait()
+  end
+end
+
+
 
